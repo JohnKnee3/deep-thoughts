a964ed992dfc917af9b3f2bb1927acf88a817256
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-02 14:17:07 -0600

    21.1 zip squish

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,18 @@
-const { User, Thought } = require("../models");
+const { User, Thought } = require('../models');
 
 const resolvers = {
   Query: {
     users: async () => {
       return User.find()
-        .select("-__v -password")
-        .populate("thoughts")
-        .populate("friends");
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
     },
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select("-__v -password")
-        .populate("friends")
-        .populate("thoughts");
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
     },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -20,8 +20,8 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
-    },
-  },
+    }
+  }
 };
 
 module.exports = resolvers;
