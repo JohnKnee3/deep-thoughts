746471719b3aa48e8a4dbb34220b344e1ca6f7a6
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-04 11:44:59 -0600

    21.3 zip squish

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 import {
-  ApolloProvider,
   ApolloClient,
   InMemoryCache,
+  ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
+} from '@apollo/client';
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import Home from "./pages/Home";
+import Home from './pages/Home';
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
