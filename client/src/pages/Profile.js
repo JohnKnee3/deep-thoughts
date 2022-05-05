676089788629f9b24f036304f34f2e25e9d2fc6b
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-05 13:35:04 -0600

    got v6 running

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
 
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
+import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
@@ -19,7 +19,7 @@ const Profile = (props) => {
 
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Redirect to="/profile" />;
+    return <Navigate to="/profile" />;
   }
 
   if (loading) {
@@ -39,7 +39,7 @@ const Profile = (props) => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
       </div>
 
